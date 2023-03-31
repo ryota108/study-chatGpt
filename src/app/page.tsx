bd6e096ce253e0f490ecd31ee39d0e2ba7877c38
Ryota <aya.ryo.718@outlook.jp>
2023-03-31 23:53:21 +0900

    heroIconの導入 Topページの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,55 @@
+import {
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+      <h1 className="font-bold text-5xl  mb-20 ">ChatGPT</h1>
+
+      <div className="flex space-x-2 text-center">
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-5 w-5 text-white" />
+            <h2>Examples</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Explain Something to me</p>
+            <p className="infoText">
+              What is the difference between a dog and a cat
+            </p>
+            <p className="infoText">What is the color of the sunn ?</p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <BoltIcon className="h-5 w-5 text-white" />
+            <h2>Capabilities</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Change the ChatGPT Model to use</p>
+            <p className="infoText">
+              Messages are stored in Firebase's Firestore
+            </p>
+            <p className="infoText">What is the color of the sunn ?</p>
+          </div>
+        </div>
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <ExclamationTriangleIcon className="h-5 w-5 text-white" />
+            <h2>Limitations</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">Explain Something to me</p>
+            <p className="infoText">
+              What is the difference between a dog and a cat
+            </p>
+            <p className="infoText">What is the color of the sunn ?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
